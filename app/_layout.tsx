ff43b9118d6f456eb0a7d8b1794ba280a2c960ae
Josip Ćavar <josipcavar10@gmail.com>
2026-06-17 09:47:36 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -83,7 +83,6 @@ export default function RootLayout() {
         <Stack.Screen name="stats-chart" />
         <Stack.Screen name="challenges" />
         <Stack.Screen name="friends" />
-        <Stack.Screen name="onboarding" />
       </Stack>
     </ThemeProvider>
   );
